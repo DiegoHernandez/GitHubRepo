987561116fbb16c8d26d87356ff9491188042155
DIEGO <dghg-91@hotmail.com>
2014-01-02 22:21:33 -0600

    Primer commit

---
+++ CupBucks/src/JSONParser/ConnConf.java
@@ -0,0 +1,19 @@
+package JSONParser;
+
+public class ConnConf {
+	public static String path="https://api.foursquare.com/v2/venues/search";
+
+	// JSON Node names
+	public static final String CLIENT_ID = "B3TGNACVGPVOBSGW3NUZFX5N433W2F20SMS33OEUPRSIENOM";
+	public static final String CLIENT_SECRET = "HKPRRQ4ANAJR4XMSSEURSPBOYIW3WC231WDGAZV0YQSMEIIT";
+	
+	public static final String TAG_META = "meta";
+	public static final String TAG_CODE = "code";
+	public static final String TAG_RESPONSE = "response";
+	public static final String TAG_VENUES = "venues";
+	public static final String TAG_NAME = "name";
+	public static final String TAG_LOCATION = "location";
+	public static final String TAG_ADDRESS = "address";
+	public static final String TAG_LAT = "lat";
+	public static final String TAG_LNG = "lng";
+}
---
+++ CupBucks/src/com/joseluishdz/cupbucks/MainActivity.java
@@ -0,0 +1,234 @@
+package com.joseluishdz.cupbucks;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.joseluishdz.cupbucks.R;
+
+import Classes.GPSTracker;
+import Classes.Store;
+import JSONParser.ConnConf;
+import JSONParser.JSONParser;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends Activity {
+
+	JSONParser jParser = new JSONParser();
+	private ProgressDialog pDialog;
+	private boolean bandDone,bandErrorConn;
+	private List<Store> stores = new ArrayList<Store>();
+	
+	// GPSTracker class
+    private GPSTracker gps;
+	
+	private LocationManager locationManager;
+	private LatLng currentLoc;
+	
+	// Google Map
+    private GoogleMap googleMap;
+    
+    Calendar ca = Calendar.getInstance();
+	private String dia = Integer.toString(ca.get(Calendar.DATE));
+	private String mes = Integer.toString((ca.get(Calendar.MONTH)+1));
+	private String anio = Integer.toString(ca.get(Calendar.YEAR));
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // create class object
+        gps = new GPSTracker(MainActivity.this);
+
+        // check if GPS enabled     
+        if(gps.canGetLocation()){
+             
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            
+            currentLoc = new LatLng(latitude,longitude);
+        }else{
+        	alertError("Error de conexion por favor verifique si esta conectado a internet");
+        }
+        
+        System.out.println("HolaLatitud: " + currentLoc.latitude);
+        System.out.println("HolaLong: " + currentLoc.longitude);
+        
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+              
+        // Move the camera instantly to hamburg with a zoom of 15.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 15));
+
+        // Zoom in, animating the camera.
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
+        
+        new getStarbucksStores().execute();
+    }
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+	
+	public class getStarbucksStores extends AsyncTask<String, String, String> {
+	    private String content =  null;
+	    private boolean error = false;
+	    private String success;
+		
+	    protected void onPreExecute() {
+	    	super.onPreExecute();
+	        bandErrorConn=false;
+	        pDialog = new ProgressDialog(MainActivity.this);
+	        pDialog.setMessage("Loading, please wait...");
+	        pDialog.setIndeterminate(false);
+	        pDialog.setCancelable(false);
+	        pDialog.show();
+	    }
+	 
+	    protected String doInBackground(String... urls) {
+	        try {
+	        	//int version=Integer.parseInt(versionDB);
+	        	bandDone=true;
+	        	
+	        	if(Integer.parseInt(dia)<10){
+	        		dia="0"+Integer.parseInt(dia);
+	        	}
+	        	if(Integer.parseInt(mes)<10){
+	        		mes="0"+Integer.parseInt(mes);
+	        	}
+	        	if(Integer.parseInt(anio)<10){
+	        		anio="0"+Integer.parseInt(anio);
+	        	}
+	        	
+	        	System.out.println("holaFecha: " + anio+mes+dia);
+	        	List<NameValuePair> params = new ArrayList<NameValuePair>();
+        		params.add(new BasicNameValuePair("client_id", ConnConf.CLIENT_ID));
+        		params.add(new BasicNameValuePair("client_secret", ConnConf.CLIENT_SECRET));
+        		params.add(new BasicNameValuePair("ll", currentLoc.latitude+","+currentLoc.longitude));
+        		params.add(new BasicNameValuePair("query", "starbucks"));
+        		params.add(new BasicNameValuePair("v", anio+mes+dia));
+	        	
+        		//get distances
+    			JSONObject json = jParser.makeHttpRequest(ConnConf.path, "GET", params);
+    			System.out.println("JSON: " + json.toString());
+    			JSONObject meta = json.getJSONObject(ConnConf.TAG_META);
+    			
+    			success = meta.getString(ConnConf.TAG_CODE);
+        		
+    			System.out.println("hola1: " + success);
+    			
+                if (success.equals("200")) {
+                	JSONObject response = json.getJSONObject(ConnConf.TAG_RESPONSE);
+                	
+                	System.out.println("hola2: " + response.toString());
+                	
+                	JSONArray venuesArray = response.getJSONArray(ConnConf.TAG_VENUES);
+                	
+                	// looping through All major
+                    for (int i = 0; i < venuesArray.length(); i++) {
+                    	JSONObject venue = venuesArray.getJSONObject(i);
+                    	JSONObject location = venue.getJSONObject(ConnConf.TAG_LOCATION);
+                    	
+                    	System.out.println("hola3: " + location.toString());
+                    	
+                    	String name = venue.getString(ConnConf.TAG_NAME);
+                    	String address = location.getString(ConnConf.TAG_ADDRESS);
+                    	double lat = location.getDouble(ConnConf.TAG_LAT);
+                    	double lng = location.getDouble(ConnConf.TAG_LNG);
+                    	
+                    	
+                    	
+                    	stores.add(new Store(lat,lng,name,address));
+                    }
+                }else{
+                	bandDone=false;
+                }
+	        }catch (Exception e) {
+	            content = e.getMessage();
+	            bandErrorConn=true;
+	            cancel(true);
+	        }
+	 
+	        return null;
+	    }
+	    
+	    protected void onPostExecute(String content) {
+	    	// dismiss the dialog
+	    	pDialog.dismiss();
+	    	if(bandErrorConn==false){
+	    		if(bandDone==true){
+	            	Toast toast = Toast.makeText(getApplicationContext(), "Tienas starbucks obtenidas con exito", Toast.LENGTH_SHORT);
+					toast.show();
+
+					createMarkers();
+	            }else{
+	            	alertError("Ha ocurrido un error al intentar obtener los tiempos");
+	            }
+	    	}else{
+	    		alertError("Error al intentar obtener los tiempos");
+	    	}
+	    }
+	    
+	}
+
+	public void createMarkers(){
+		
+		for(int i=0;i<stores.size();i++){
+			
+			Marker kiel = googleMap.addMarker(new MarkerOptions()
+	        .position(new LatLng(stores.get(i).getLat(),stores.get(i).getLng()))
+	        .title(stores.get(i).getName())
+	        .snippet(stores.get(i).getAddress())
+	        .icon(BitmapDescriptorFactory
+	        .fromResource(R.drawable.marker_icon_4)));
+		}
+	}
+	
+	public void alertError(String msg){
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(msg)
+		        .setIcon(R.drawable.ic_launcher)
+				.setTitle("Error")
+		        .setCancelable(false)
+		        .setNegativeButton("Aceptar",
+		                new DialogInterface.OnClickListener() {
+		                    public void onClick(DialogInterface dialog, int id) {
+		                    	dialog.cancel();
+		                    }
+		                });
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+}
